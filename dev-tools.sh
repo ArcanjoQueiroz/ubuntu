fe64dfb3d117a69ba80c7f64d449a1df0b63550a
Alexandre Arcanjo de Queiroz <alexandre.queiroz@live.com>
2019-08-11 13:43:21 -0300

    Adding antlr4

--- dev-tools.sh
+++ dev-tools.sh
@@ -19,4 +19,31 @@ alias google-format=googleFormat' >> ~/.bashrc
     fi
 }
 
+function install_antlr() {
+    mkdir -p ${HOME}/bin && \
+        wget https://www.antlr.org/download/antlr-4.7.2-complete.jar && \
+        mv antlr-4.7.2-complete.jar ${HOME}/bin/antlr.jar
+    ALIAS=$(cat ~/.bashrc | grep 'alias antlr=')
+    if [ -z "${ALIAS}" ]; then
+        echo '
+function runAntlr() {
+    LANGUAGE=Java
+    if [ "$#" -eq 1 ]; then
+        FILTER=$1
+    elif [ "$#" -eq 2 ]; then
+        FILTER=$1
+        LANGUAGE=$2
+    else
+        FILTER=*.g4
+    fi
+    find . -type f -name ${FILTER} -exec java -jar ${HOME}/bin/antlr.jar -Dlanguage=${LANGUAGE} {} \;
+    if [ "${LANGUAGE}" == "Java" ]; then
+        javac -cp ${HOME}/bin/antlr.jar *.java
+    fi
+}
+alias antlr=runAntlr' >> ~/.bashrc
+    fi
+}
+
 install_google_java_format
+install_antlr
