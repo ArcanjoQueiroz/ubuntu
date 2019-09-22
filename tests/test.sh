46a4daa5366d6bb26e2fe8710565808f1ebb141b
Alexandre Arcanjo de Queiroz <alexandre.queiroz@live.com>
2019-09-21 21:34:06 -0300

    Fix test script

--- tests/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT")
-
-cp ${SCRIPT_PATH}/../install.sh ${SCRIPT_PATH}
-docker build -t ubuntu-script:1.0.1-ubuntu-19.04 .
-rm install.sh
