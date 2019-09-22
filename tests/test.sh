337309ada0e339b04edd33f163cd5297a697b2f5
Alexandre Arcanjo de Queiroz <alexandre.queiroz@live.com>
2019-09-21 21:31:45 -0300

    Fix directory name

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cp ../install.sh .
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+cp ${SCRIPT_PATH}/../install.sh ${SCRIPT_PATH}
 docker build -t ubuntu-script:1.0.1-ubuntu-19.04 .
 rm install.sh
